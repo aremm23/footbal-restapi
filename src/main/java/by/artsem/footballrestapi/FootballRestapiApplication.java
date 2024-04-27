a267b258f36c6bfffb40586339de2f16fe1d6e58
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-04-27 09:13:55 +0300

    Remove swagger from main method

--- src/main/java/by/artsem/footballrestapi/FootballRestapiApplication.java
+++ src/main/java/by/artsem/footballrestapi/FootballRestapiApplication.java
@@ -7,13 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(
-        title = "Api about football players, clubs and brands.",
-        description = "Contact:",
-        contact = @Contact(name = "Artsem Maiseyenka",
-                url = "https://github.com/aremm23",
-                email = "artsem.maiseyenka@gmail.com")
-))
 public class FootballRestapiApplication {
 
     public static void main(String[] args) {
