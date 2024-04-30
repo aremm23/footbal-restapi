f7dc8c705d29fadde9e72df63d4d834a51c3d381
Artsem Maiseyenka <artsem.maiseyenka@gmail.com>
2024-04-30 11:41:11 +0300

    Add more test BrandServiceTest
    Coverage of BrandServiceTest is 100%

--- src/test/java/by/artsem/footballrestapi/services/BrandServiceTest.java
+++ src/test/java/by/artsem/footballrestapi/services/BrandServiceTest.java
@@ -5,6 +5,7 @@ import by.artsem.footballrestapi.exceptions.DataNotFoundedException;
 import by.artsem.footballrestapi.models.Brand;
 import by.artsem.footballrestapi.models.Player;
 import by.artsem.footballrestapi.repository.BrandRepository;
+import by.artsem.footballrestapi.repository.PlayerRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,6 +24,9 @@ import static org.mockito.BDDMockito.given;
 class BrandServiceTest {
     @Mock
     private BrandRepository brandRepository;
+
+    @Mock
+    private PlayerRepository playerRepository;
 
     @InjectMocks
     private BrandService brandService;
@@ -55,7 +59,9 @@ class BrandServiceTest {
 
         given(brandRepository.existsByName("TestBrand1")).willReturn(Boolean.TRUE);
 
-        assertThrows(DataNotCreatedException.class, () -> brandService.saveBrand(testBrand1));
+        assertThrows(DataNotCreatedException.class,
+                () -> brandService.saveBrand(testBrand1)
+        );
     }
 
     @Test
@@ -89,6 +95,35 @@ class BrandServiceTest {
         assertEquals(testPlayer2.getBrands(), Collections.emptyList());
     }
 
+    @Test
+    public void BrandService_RemoveNotExist_ThrowsDataNotFoundedException() {
+        given(brandRepository.findById(1L)).willReturn(Optional.empty());
+
+        assertThrows(DataNotFoundedException.class,
+                () -> brandService.removeBrand(1L)
+        );
+    }
+
+    @Test
+    public void BrandService_GetAll_ReturnsListOfBrands() {
+        init();
+        List<Brand> brands = List.of(testBrand1,
+                Brand.builder().id(2L).name("TestBrand2").players(Collections.emptyList()).build()
+        );
+        given(brandRepository.findAll()).willReturn(brands);
+
+        assertEquals(brandService.getBrands(), brands);
+    }
+
+    @Test
+    public void BrandService_GetAll_EmptyList() {
+        init();
+        List<Brand> brands = Collections.emptyList();
+        given(brandRepository.findAll()).willReturn(brands);
+
+        assertEquals(brandService.getBrands(), brands);
+    }
+
 
     @Test
     public void BrandService_FindById_ReturnsExpectedBrand() {
@@ -109,6 +144,92 @@ class BrandServiceTest {
 
         brandService.findById(testBrand1.getId());
 
-        assertThrows(DataNotFoundedException.class, () -> brandService.findById(2L));
+        assertThrows(DataNotFoundedException.class,
+                () -> brandService.findById(2L)
+        );
     }
+
+    @Test
+    public void BrandService_UpdateName_ChangesBrandName() {
+        init();
+        Brand testBrand2 = Brand.builder().id(1L).name("TestBrand2").players(Collections.emptyList()).build();
+        given(brandRepository.findById(testBrand1.getId())).willReturn(Optional.of(testBrand1));
+
+        brandService.updateName(testBrand1.getId(), testBrand2);
+
+        assertEquals(testBrand1.getName(), testBrand2.getName());
+    }
+
+    @Test
+    public void BrandService_UpdateName_ThrowsDataNotFoundedException() {
+        init();
+        Brand testBrand2 = Brand.builder().id(1L).name("TestBrand2").players(Collections.emptyList()).build();
+        given(brandRepository.findById(testBrand1.getId())).willReturn(Optional.empty());
+
+        assertThrows(DataNotFoundedException.class,
+                () -> brandService.updateName(testBrand1.getId(), testBrand2)
+        );
+    }
+
+    @Test
+    public void BrandService_AddExistPlayer_SuccessfullyAddPlayerToBrand() {
+        init();
+
+        given(brandRepository.findById(testBrand1.getId())).willReturn(Optional.of(testBrand1));
+        given(playerRepository.findByName(testPlayer1.getName())).willReturn(Optional.of(testPlayer1));
+
+        brandService.addExistPlayer(testBrand1.getId(), testPlayer1.getName());
+
+        assertEquals(testPlayer1.getBrands().get(0), testBrand1);
+    }
+
+    @Test
+    public void BrandService_AddExistPlayer_ThrowBrandNotFound() {
+        init();
+
+        given(brandRepository.findById(testBrand1.getId())).willReturn(Optional.empty());
+
+        assertThrows(DataNotFoundedException.class,
+                () -> brandService.addExistPlayer(testBrand1.getId(), testPlayer1.getName())
+        );
+    }
+
+    @Test
+    public void BrandService_AddExistPlayer_ThrowPlayerNotFound() {
+        init();
+
+        given(playerRepository.findByName(testPlayer1.getName())).willReturn(Optional.empty());
+        given(brandRepository.findById(testBrand1.getId())).willReturn(Optional.of(testBrand1));
+
+        assertThrows(DataNotFoundedException.class,
+                () -> brandService.addExistPlayer(testBrand1.getId(), testPlayer1.getName())
+        );
+    }
+
+
+    @Test
+    public void BrandService_FindByName_ReturnsExpectedBrand() {
+        init();
+        testBrand1.setPlayers(Collections.emptyList());
+        given(brandRepository.findByName(testBrand1.getName())).willReturn(Optional.empty());
+
+        assertThrows(DataNotFoundedException.class,
+                () -> brandService.findByName(testBrand1.getName())
+        );
+    }
+
+
+    @Test
+    public void BrandService_FindAll() {
+        init();
+        testBrand1.setPlayers(Collections.emptyList());
+        Brand testBrand2 = Brand.builder().id(1L).name("TestBrand2").players(Collections.emptyList()).build();
+        List<Brand> brands = List.of(testBrand1, testBrand2);
+        List<String> brandsNames = List.of(testBrand1.getName(), testBrand2.getName());
+        given(brandRepository.findByName(testBrand1.getName())).willReturn(Optional.of(testBrand1));
+        given(brandRepository.findByName(testBrand2.getName())).willReturn(Optional.of(testBrand2));
+
+        assertEquals(brands, brandService.findByNames(brandsNames));
+    }
+
 }
